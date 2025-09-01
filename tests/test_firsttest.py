fa2af6b433a103df1ec9094ffe68d8e06c938270
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2025-09-01 13:24:46 +0200

    Update test_firsttest.py

--- tests/test_firsttest.py
+++ tests/test_firsttest.py
@@ -2,9 +2,8 @@ from pages.main_page import mainPage
 from typing import Dict
 
 
-def test_boards(set_up, main_url: str, account_data: Dict[str,str]) -> None:
+def test_boards(set_up, capture_bad_responses) -> None:
     page = set_up
     main_page = MainPage(page)
 
-    main_page.open_main_page(main_url)
-    main_page.sign_in(account_data, main_url)
+    # steps in test
