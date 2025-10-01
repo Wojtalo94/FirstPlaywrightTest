f0c9b9f54efb28c4e85f09a855ba9c8c357fbf55
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2025-10-01 07:15:37 +0200

    Update helpers.py

--- .vscode/helpers/helpers.py
+++ .vscode/helpers/helpers.py
@@ -15,3 +15,10 @@ def date_months_back(months_back: int) -> str:
 def date_months_forward(months_forward: int) -> str:
     target_date = date.today() + relativedelta(months=months_forward)
     return target_date.strftime('%d/%m/%Y')
+
+
+def normalize_string(s: str) -> str:
+    s = s.strip()
+    s = re.sub(r"\s*-\s*", "-", s)
+    s = re.sub(r"\s+", " ", s)
+    return s
